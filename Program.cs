106d568d5410aff95252ec768a52fb8bf865a8dd
Hafizh Budiman <hafizh.budiman@gmail.com>
2018-03-17 06:57:19 +0700

    Added BFS and DFS implementation

--- Program.cs
+++ Program.cs
@@ -9,18 +9,34 @@ namespace TUBES_STIMA_2
     static class Program
     {
         static void Main(){
-            Graph g = new Graph(6);
-            g.addEdge(5, 2);
-            g.addEdge(5, 0);
-            g.addEdge(4, 0);
-            g.addEdge(4, 1);
+            Graph g = new Graph(5);
+            g.addEdge(2, 0);
             g.addEdge(2, 3);
+            g.addEdge(0, 1);
+            g.addEdge(0, 3);
             g.addEdge(3, 1);
+            g.addEdge(3, 4);
+            g.addEdge(1, 4);
 
-            Console.WriteLine("BFS Topological Sort Result: \n");
-            List<int> res = g.topologicalSortBFS();
-            for (int i = 0; i < res.Count; i++)
-                Console.Write(res[i] + " ");
+            Console.Clear();
+
+            // BFS IMPLEMENTATION
+            Console.Write("\nRunning BFS Topological Algorithm...\n");
+            List<int> result = g.topologicalSortBFS();
+            Console.Write("\nBFS Topological Sort Result: (");
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (i != 0) Console.Write(",");
+                Console.Write(result[i]);
+            }
+            Console.Write(")\n");
+
+            // DFS IMPLEMENTATION
+            Console.Write("\nRunning DFS Topological Algorithm...\n");
+            List<int>[] res = g.topologicalSortDFS();
+            Console.WriteLine("\nDFS Topological Sort Result: \n");
+            for (int i = 0; i < res.Length; i++) 
+                Console.Write((res[i])[0] + " Timestamp(start/stop): (" + (res[i])[1] + "/" + (res[i])[2] + ")\n\n");
         }
     }
 }
