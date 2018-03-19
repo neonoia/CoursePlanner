358c4cee5ead00480a8078e93cc314259e4758cd
Pollycarpus <fijarmayoza.rabbi@gmail.com>
2018-03-20 00:27:06 +0700

    Throw exception

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace CoursePlanner
 {
@@ -25,7 +26,7 @@ namespace CoursePlanner
 
             outputBox.AppendText("Hello and Welcome! ");
             outputBox.AppendText("This program will help you plan and sort your college course!\n");
-            outputBox.AppendText("Made with heart by Blek Panther Team :\n");
+            outputBox.AppendText("Made with heart by Blek Panter Team :\n");
             outputBox.AppendText("13516081 - Rabbi Fijar Mayoza\n");
             outputBox.AppendText("13516106 - Kurniandha Sukma Yunastrian\n");
             outputBox.AppendText("13516137 - Hafizh Budiman\n");
@@ -43,21 +44,37 @@ namespace CoursePlanner
 
             // BFS IMPLEMENTATION
             outputBox.AppendText("Running BFS Topological Algorithm...\n");
-            List<int> result = gr.topologicalSortBFS();
-            outputBox.AppendText("BFS Topological Sort Result: (");
-            for (int i = 0; i < result.Count; i++)
+            try           
             {
-                if (i != 0) outputBox.AppendText(",");
-                outputBox.AppendText(result[i].ToString());
+                List<int> result = gr.topologicalSortBFS();
+                outputBox.AppendText("BFS Topological Sort Result: (");
+                for (int i = 0; i < result.Count; i++)
+                {
+                    if (i != 0) outputBox.AppendText(",");
+                    outputBox.AppendText(result[i].ToString());
+                }
+                outputBox.AppendText(")\n");
             }
-            outputBox.AppendText(")\n");
+            catch(NullReferenceException ne)
+            {
+                outputBox.AppendText("No result\n");
+            }
+           
 
             // DFS IMPLEMENTATION
             outputBox.AppendText("Running DFS Topological Algorithm...\n");
-            List<int>[] res = gr.topologicalSortDFS();
-            outputBox.AppendText("DFS Topological Sort Result: \n");
-            for (int i = 0; i < res.Length; i++)
-                outputBox.AppendText((res[i])[0] + " Timestamp(start/stop): (" + (res[i])[1] + "/" + (res[i])[2] + ")\n");
+
+            try
+            {
+                List<int>[] res = gr.topologicalSortDFS();
+                outputBox.AppendText("DFS Topological Sort Result: \n");
+                for (int i = 0; i < res.Length; i++)
+                    outputBox.AppendText((res[i])[0] + " Timestamp(start/stop): (" + (res[i])[1] + "/" + (res[i])[2] + ")\n");
+            }
+            catch(NullReferenceException en)
+            {
+                outputBox.AppendText("No result\n");
+            }
 
             Window1 subWindow = new Window1(FileName);
             subWindow.Show();
@@ -74,119 +91,129 @@ namespace CoursePlanner
             int num;
             int course_now = 0;
 
-            // Count the total courses
-            System.IO.StreamReader file = new System.IO.StreamReader(FileName);
-            while ((line = file.ReadLine()) != null)
+            try
             {
-                ammount++;
-            }
-            file.Close();
-            Graph g = new Graph(ammount);
-            string[] course_code = new string[ammount];
-
-            // Read each course code
-            System.IO.StreamReader files = new System.IO.StreamReader(FileName);
-            while (!files.EndOfStream)
-            {
-                ch = (char)files.Read();
-                if (ch != '.')
+                // Count the total courses
+                System.IO.StreamReader file = new System.IO.StreamReader(FileName);
+                while ((line = file.ReadLine()) != null)
                 {
-                    if (ch != ',')
+                    ammount++;
+                }
+                file.Close();
+
+                Graph g = new Graph(ammount);
+                string[] course_code = new string[ammount];
+
+                // Read each course code
+                System.IO.StreamReader files = new System.IO.StreamReader(FileName);
+                while (!files.EndOfStream)
+                {
+                    ch = (char)files.Read();
+                    if (ch != '.')
                     {
-                        temp1.Append(ch);
+                        if (ch != ',')
+                        {
+                            temp1.Append(ch);
+                        }
+                        else
+                        {
+                            course_code[course_now] = temp1.ToString();
+                            course_now++;
+                            temp1.Length = 0;
+                            while (ch != '.')
+                            {
+                                ch = (char)files.Read();
+                            }
+                            ch = (char)files.Read();
+                            ch = (char)files.Read();
+                        }
                     }
                     else
                     {
                         course_code[course_now] = temp1.ToString();
                         course_now++;
                         temp1.Length = 0;
-                        while (ch != '.')
-                        {
-                            ch = (char)files.Read();
-                        }
                         ch = (char)files.Read();
                         ch = (char)files.Read();
                     }
                 }
-                else
+                files.Close();
+                outputBox.AppendText("Course Code Number : \n");
+                for (course_now = 0; course_now < ammount; course_now++)
                 {
-                    course_code[course_now] = temp1.ToString();
-                    course_now++;
-                    temp1.Length = 0;
-                    ch = (char)files.Read();
-                    ch = (char)files.Read();
+                    outputBox.AppendText(course_now.ToString());
+                    outputBox.AppendText(" = ");
+                    outputBox.AppendText(course_code[course_now].ToString());
+                    outputBox.AppendText("\n");
                 }
-            }
-            files.Close();
-            outputBox.AppendText("Course Code Number : \n");
-            for (course_now = 0; course_now < ammount; course_now++)
-            {
-                outputBox.AppendText(course_now.ToString());
-                outputBox.AppendText(" = ");
-                outputBox.AppendText(course_code[course_now].ToString());
-                outputBox.AppendText("\n");
-            }
-            // Read each course code pre-requisite
-            course_now = 0;
-            int sign = 0;
-            System.IO.StreamReader filesn = new System.IO.StreamReader(FileName);
-            while (!filesn.EndOfStream)
-            {
-                ch = (char)filesn.Read();
-                if (ch != '.')
+                // Read each course code pre-requisite
+                course_now = 0;
+                int sign = 0;
+                System.IO.StreamReader filesn = new System.IO.StreamReader(FileName);
+                while (!filesn.EndOfStream)
                 {
-                    if (ch != ',')
+                    ch = (char)filesn.Read();
+                    if (ch != '.')
                     {
-                        temp2.Append(ch);
+                        if (ch != ',')
+                        {
+                            temp2.Append(ch);
+                        }
+                        else
+                        {
+                            if (sign == 0)
+                            {
+                                sign = 1;
+                                temp2.Length = 0;
+                            }
+                            else
+                            {
+                                for (num = 0; num < ammount; num++)
+                                {
+                                    if (string.Equals(course_code[num], temp2.ToString()))
+                                    {
+                                        g.addEdges(num, course_now);
+                                        num = ammount + 1;
+                                    }
+                                }
+                                temp2.Length = 0;
+                            }
+                            ch = (char)filesn.Read();
+                        }
                     }
                     else
                     {
                         if (sign == 0)
                         {
-                            sign = 1;
                             temp2.Length = 0;
+                            course_now++;
                         }
                         else
                         {
+                            sign = 0;
                             for (num = 0; num < ammount; num++)
                             {
-                                if (string.Equals(course_code[num], temp2.ToString()))
+
+                                if (string.Compare(course_code[num], temp2.ToString()) == 0)
                                 {
                                     g.addEdges(num, course_now);
                                     num = ammount + 1;
                                 }
                             }
                             temp2.Length = 0;
+                            course_now++;
                         }
-                        ch = (char)filesn.Read();
                     }
                 }
-                else
-                {
-                    if (sign == 0)
-                    {
-                        temp2.Length = 0;
-                        course_now++;
-                    }
-                    else
-                    {
-                        sign = 0;
-                        for (num = 0; num < ammount; num++)
-                        {
 
-                            if (string.Compare(course_code[num], temp2.ToString()) == 0)
-                            {
-                                g.addEdges(num, course_now);
-                                num = ammount + 1;
-                            }
-                        }
-                        temp2.Length = 0;
-                        course_now++;
-                    }
-                }
+                filesn.Close();
+                return g;
+            } catch (FileNotFoundException e)
+            {
+                Console.WriteLine("[Data File Missing] {0}", e);
+                outputBox.AppendText("File Not Found !\n");
+                return null;
             }
-            filesn.Close();
-            return g;
         }
     }
 }
--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 using QuickGraph;
 
 namespace CoursePlanner
@@ -38,23 +39,30 @@ namespace CoursePlanner
             var g = new BidirectionalGraph<object, IEdge<object>>();
 
             //add the vertices to the graph
-            string[] vertices = new string[gr.getVertice()];
-            for (int i = 0; i < gr.getVertice(); i++)
+            try
             {
-                vertices[i] = i.ToString();
-                g.AddVertex(vertices[i]);
-            }
-
-            //add some edges to the graph
-            for (int i=0; i<gr.getVertice(); i++){
-                for(int j=0; j<gr.getAdjIdxLength(i); j++){
-                    g.AddEdge(new Edge<object>(vertices[i], vertices[gr.getAdj(i,j)]));
+                string[] vertices = new string[gr.getVertice()];
+                for (int i = 0; i < gr.getVertice(); i++)
+                {
+                    vertices[i] = i.ToString();
+                    g.AddVertex(vertices[i]);
                 }
+
+                //add some edges to the graph
+                for (int i = 0; i < gr.getVertice(); i++)
+                {
+                    for (int j = 0; j < gr.getAdjIdxLength(i); j++)
+                    {
+                        g.AddEdge(new Edge<object>(vertices[i], vertices[gr.getAdj(i, j)]));
+                    }
+                }
+
+                _graphToVisualize = g;
+                InitializeComponent();
             }
-
-            _graphToVisualize = g;
-
-            InitializeComponent();
+            catch (NullReferenceException ne)
+            {
+            }
         }
 
         private Graph ReadFile(string FileName)
@@ -69,110 +77,118 @@ namespace CoursePlanner
             int course_now = 0;
 
             // Count the total courses
-            System.IO.StreamReader file = new System.IO.StreamReader(FileName);
-            while ((line = file.ReadLine()) != null)
+            try
             {
-                ammount++;
-            }
-            file.Close();
-            Graph g = new Graph(ammount);
-            string[] course_code = new string[ammount];
-
-            // Read each course code
-            System.IO.StreamReader files = new System.IO.StreamReader(FileName);
-            while (!files.EndOfStream)
-            {
-                ch = (char)files.Read();
-                if (ch != '.')
+                System.IO.StreamReader file = new System.IO.StreamReader(FileName);
+                while ((line = file.ReadLine()) != null)
                 {
-                    if (ch != ',')
+                    ammount++;
+                }
+                file.Close();
+                Graph g = new Graph(ammount);
+                string[] course_code = new string[ammount];
+
+                // Read each course code
+                System.IO.StreamReader files = new System.IO.StreamReader(FileName);
+                while (!files.EndOfStream)
+                {
+                    ch = (char)files.Read();
+                    if (ch != '.')
                     {
-                        temp1.Append(ch);
+                        if (ch != ',')
+                        {
+                            temp1.Append(ch);
+                        }
+                        else
+                        {
+                            course_code[course_now] = temp1.ToString();
+                            course_now++;
+                            temp1.Length = 0;
+                            while (ch != '.')
+                            {
+                                ch = (char)files.Read();
+                            }
+                            ch = (char)files.Read();
+                            ch = (char)files.Read();
+                        }
                     }
                     else
                     {
                         course_code[course_now] = temp1.ToString();
                         course_now++;
                         temp1.Length = 0;
-                        while (ch != '.')
-                        {
-                            ch = (char)files.Read();
-                        }
                         ch = (char)files.Read();
                         ch = (char)files.Read();
                     }
                 }
-                else
-                {
-                    course_code[course_now] = temp1.ToString();
-                    course_now++;
-                    temp1.Length = 0;
-                    ch = (char)files.Read();
-                    ch = (char)files.Read();
-                }
-            }
-            files.Close();
+                files.Close();
 
-            // Read each course code pre-requisite
-            course_now = 0;
-            int sign = 0;
-            System.IO.StreamReader filesn = new System.IO.StreamReader(FileName);
-            while (!filesn.EndOfStream)
-            {
-                ch = (char)filesn.Read();
-                if (ch != '.')
+                // Read each course code pre-requisite
+                course_now = 0;
+                int sign = 0;
+                System.IO.StreamReader filesn = new System.IO.StreamReader(FileName);
+                while (!filesn.EndOfStream)
                 {
-                    if (ch != ',')
+                    ch = (char)filesn.Read();
+                    if (ch != '.')
                     {
-                        temp2.Append(ch);
+                        if (ch != ',')
+                        {
+                            temp2.Append(ch);
+                        }
+                        else
+                        {
+                            if (sign == 0)
+                            {
+                                sign = 1;
+                                temp2.Length = 0;
+                            }
+                            else
+                            {
+                                for (num = 0; num < ammount; num++)
+                                {
+                                    if (string.Equals(course_code[num], temp2.ToString()))
+                                    {
+                                        g.addEdges(num, course_now);
+                                        num = ammount + 1;
+                                    }
+                                }
+                                temp2.Length = 0;
+                            }
+                            ch = (char)filesn.Read();
+                        }
                     }
                     else
                     {
                         if (sign == 0)
                         {
-                            sign = 1;
                             temp2.Length = 0;
+                            course_now++;
                         }
                         else
                         {
-                            for (num = 0; num < ammount;num++) 
+                            sign = 0;
+                            for (num = 0; num < ammount; num++)
                             {
-                                if (string.Equals(course_code[num], temp2.ToString())) {
+
+                                if (string.Compare(course_code[num], temp2.ToString()) == 0)
+                                {
                                     g.addEdges(num, course_now);
                                     num = ammount + 1;
                                 }
                             }
                             temp2.Length = 0;
+                            course_now++;
                         }
-                        ch = (char)filesn.Read();
                     }
                 }
-                else
-                {
-                    if (sign == 0)
-                    {
-                        temp2.Length = 0;
-                        course_now++;
-                    }
-                    else
-                    {
-                        sign = 0;
-                        for (num = 0; num < ammount; num++)
-                        {
-                            
-                            if (string.Compare(course_code[num],temp2.ToString()) == 0)
-                            {
-                                g.addEdges(num, course_now);
-                                num = ammount + 1;
-                            }
-                        }
-                        temp2.Length = 0;
-                        course_now++;
-                    }
-                }
+                filesn.Close();
+                return g;
             }
-            filesn.Close();
-            return g;
+            catch(FileNotFoundException e)
+            {
+                return null;
+            }
         }
     }
 }
